dfb04bbcec2880ab70e2f832c23a4879b22d1bfe
Yangfan Zhang <42492049+YangfanZhang@users.noreply.github.com>
2020-07-14 09:38:15 +1000

    Added methods to handle complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({ fName: "", lName: "" });
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={fullName.fname}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
